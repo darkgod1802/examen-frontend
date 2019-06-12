c2e8787c38fe12484333858232673a2ac7f4ef58
darkgod1802 <bryan.choque.canaza@gmail.com>
2019-06-12 10:41:07 -0400

    Subiendo proyecto

---
+++ app/src/main/java/bo/edu/ucb/darkgod/examen/InicioSesion.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.darkgod.examen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+}
